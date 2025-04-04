ab75a3b7abc0ff01a634f763caddcc1bc0213642
chrleroy <you@example.com>
2025-04-04 15:06:03 +0200

    We've got refactor - mat2 0404 PAUL

--- test_mistral.c
+++ test_mistral.c
@@ -20,13 +20,11 @@ void print_grid(int grid[ROW_SIZE][COL_SIZE])
     printf("\n");
 }
 
-//
 static bool check_position(int grid[ROW_SIZE][COL_SIZE], int row, int col)
 {
     return (row >= 0 && row < ROW_SIZE && col >= 0 && col < COL_SIZE && grid[row][col] != 0);
 }
 
-//
 static bool can_modify(int grid[ROW_SIZE][COL_SIZE], int row, int col)
 {
     int sum = 0;
@@ -46,14 +44,11 @@ static bool can_modify(int grid[ROW_SIZE][COL_SIZE], int row, int col)
     return (adjacent_count >= 2 && sum <= MAX);
 }
 
-//
-static int	try_modify(int grid[ROW_SIZE][COL_SIZE], int row, int col, bool *modified)
+static void try_modify(int grid[ROW_SIZE][COL_SIZE], int row, int col, bool *modified, int *count)
 {
-    int original_value;
-    
-	if (can_modify(grid, row, col))
+    if (can_modify(grid, row, col))
     {
-        original_value = grid[row][col];
+        int original_value = grid[row][col];
         grid[row][col] = 0;
         *modified = true;
         for (int i = 0; i < ROW_SIZE; i++)
@@ -61,60 +56,72 @@ static int	try_modify(int grid[ROW_SIZE][COL_SIZE], int row, int col, bool *modi
             for (int j = 0; j < COL_SIZE; j++)
             {
                 if (grid[i][j] != 0)
-                    try_modify(grid, i, j, modified);
+                {
+                    bool new_modified = false;
+                    try_modify(grid, i, j, &new_modified, count);
+                    if (new_modified)
+                    {
+                        (*count)++;
+                        print_grid(grid);
+                    }
+                }
             }
         }
-		print_grid(grid);
-        if (!*modified)
-            return (grid[row][col] = original_value, 0);
-		return 1;
+        grid[row][col] = original_value;
     }
-	return (0);
 }
 
-//
-void explore_possibilities(int grid[ROW_SIZE][COL_SIZE], int *ret)
+void explore_possibilities(int grid[ROW_SIZE][COL_SIZE], int *count, int depth)
 {
-    bool modified = false;
+	bool	modified = false;
+	
+	for (int c = 0; c < depth; c++)
+	{
+		for (int i = 0; i < ROW_SIZE; i++)
+	    {
+	        for (int j = 0; j < COL_SIZE; j++)
+	        {
+	            if (grid[i][j] != 0)
+	            {
+	                modified = false;
+	                try_modify(grid, i, j, &modified, count);
+	                if (modified)
+	                {
+	                    (*count)++;
+	                    print_grid(grid);
+	                }
+	            }
+	        }
+	    }
+	}
+}
 
+void get_input(int *depth, int initial[ROW_SIZE][COL_SIZE])
+{
+    scanf("%d", depth);
     for (int i = 0; i < ROW_SIZE; i++)
     {
         for (int j = 0; j < COL_SIZE; j++)
-        {
-            if (grid[i][j] != 0)
-                *ret += try_modify(grid, i, j, &modified);
-        }
-    }
-}
-
-//Gets input from the stdin
-void	get_input(int *depth, int initial[ROW_SIZE][COL_SIZE])
-{
-    scanf("%d", depth);
-	for (int i = 0; i < ROW_SIZE; i++)
-	{
-        for (int j = 0; j < COL_SIZE; j++) 
             scanf("%d", &initial[i][j]);
     }
 }
 
-//
 int main(void)
 {
     int depth;
-	int ret = 0;
-	int count = 0;
-	int	initial[ROW_SIZE][COL_SIZE];
+    int count = 0;
+    int initial[ROW_SIZE][COL_SIZE];
 
-	get_input(&depth, initial);
+    get_input(&depth, initial);
 
-	print_grid(initial);
-
-    explore_possibilities(initial, &ret);
-	
     print_grid(initial);
 
-	printf("%d\n", ret);
+    explore_possibilities(initial, &count, depth);
+
+    print_grid(initial);
+
+    printf("Total possibilities: %d\n", count);
 
     return 0;
 }
+
